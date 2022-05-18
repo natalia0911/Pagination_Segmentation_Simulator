b596c6626483d4ae6910fa1193469f3579b8f585
JacobPG <njacobpg02@gmail.com>
2022-05-18 00:40:56 -0600

    Inicio productor

---
+++ Process.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Segments{
+    //Cantidad de segmentos: de 1 a 5
+    short cantidad;
+    /*
+    Como se sabe que siempre van a ser de 1 a 5 segmentos para no hacerlo más complicado, 
+    la estructura de segmentos contiene la cantidad de espacios para cada uno de los segmentos y
+    así no hay que recorrer una lista.
+    */
+    short espaciosSeg1;
+    short espaciosSeg2;
+    short espaciosSeg3;
+    short espaciosSeg4;
+    short espaciosSeg5;
+}Segments;
+
+
+typedef struct Process{
+    pid_t PID;
+    short burst;
+    //Se utiliza si se usa paginación.
+    short cantPaginas;
+    //Se utiliza si se usa Segmentación.
+    Segments *seg;
+
+}Process;
+
+
+Segments *createSegment(short cantidad){
+    Segments *seg = malloc(sizeof(Segments));
+    seg->cantidad = cantidad;//Cantidad de segmentos.
+    seg->espaciosSeg1=0;
+    seg->espaciosSeg2=0;
+    seg->espaciosSeg3=0;
+    seg->espaciosSeg4=0;
+    seg->espaciosSeg5=0;
+    return seg;
+}
+
+
+Process *createProcessSeg(short burst, short cantSegmentos){
+    Process *p = malloc(sizeof(Process));
+
+    Segments *seg = createSegment(cantSegmentos);
+
+    p->burst = burst;
+    p->seg = seg;
+    return p;
+}
+
+Process *createProcessPag(short burst, short cantPaginas){
+    Process *p = malloc(sizeof(Process));
+    p->burst = burst;
+    p->cantPaginas = cantPaginas;
+    return p;
+}
---
+++ Producer.c
@@ -0,0 +1,108 @@
+//Librerías  a utilizar
+#include <stdio.h>  
+#include <stdlib.h>
+#include <string.h>
+
+#include <pthread.h>
+
+//Constantes a utilizar
+#define MAXPAGES 10
+#define MAXSEGMENTS 5
+#define MAXESPACIOS 3
+#define MINPAGSEGESP 1
+
+#define MAXBURST 60
+#define MINBURST 20
+
+#define MAXTIME 60
+#define MINTIME 30
+
+#define MICROSECONDOS 1000000
+
+pid_t PID_AutoIncrement=0;
+
+//Flag que me indica que tipo de algoritmo estoy usando.
+int isSEGMENTATION = -1;
+
+//Flag que me indica que tipo de algoritmo estoy usando.
+int isRUNNING = 1;
+
+void generarDatos(){
+    short burst;
+    short espera;
+
+    short cantidadPagSeg;
+    short espaciosSeg;
+
+    Process *process;
+
+    pthread_t hiloAgregar;   
+
+    //Usar un flag para detenerlo.
+    while(isRUNNING){//De momento usa esa condición para hacer las pruebas
+
+        //ESPERA ENTRE minEspera A maxEspera
+        espera = random() % ((MAXTIME) - (MINTIME)+1) + (MINTIME);
+
+        //Usa la variable para pausar el hilo.
+        usleep (espera*MICROSECONDOS);
+        
+        burst = random() % ((MAXBURST) - (MINBURST)+1) + (MINBURST);
+
+        cantidadPagSeg = random() % ((MAXPAGES) - (MINPAGSEGESP)+1) + (MINPAGSEGESP);
+
+        //  NOTA: Falta agregar el PID
+
+        if (isSEGMENTATION==0){//Paginación
+            //Ovtiene la cantidad de paginación.
+            cantidadPagSeg = random() % ((MAXPAGES) - (MINPAGSEGESP)+1) + (MINPAGSEGESP);
+            process = createProcessPag(burst, cantidadPagSeg);
+
+        }else if (isSEGMENTATION==1){//Segmentación.
+            //Obtiene la cantidad de segmentos
+            cantidadPagSeg = random() % ((MAXSEGMENTS) - (MINPAGSEGESP)+1) + (MINPAGSEGESP);
+            process = createProcessSeg(burst, cantidadPagSeg);
+
+            for (int i=0; i<cantidadPagSeg; i++){
+                espaciosSeg = random() % ((MAXESPACIOS) - (MINPAGSEGESP)+1) + (MINPAGSEGESP);
+
+                if (i==0) {process->seg->espaciosSeg1=espaciosSeg; continue;}
+                if (i==1) {process->seg->espaciosSeg2=espaciosSeg; continue;}
+                if (i==2) {process->seg->espaciosSeg3=espaciosSeg; continue;}
+                if (i==3) {process->seg->espaciosSeg4=espaciosSeg; continue;}   
+                if (i==4) {process->seg->espaciosSeg5=espaciosSeg; continue;}
+            }
+        }
+
+        //AGREGA EL PROCESO        
+        //pthread_create (&hiloAgregar, NULL, agregarProceso, &process);
+            
+    }
+    //pthread_join (hiloAgregar, NULL );
+    return;
+
+
+int main(int argc, char *argv[]) {
+    if (argc<2){printf("ERROR: Debe ingresar el tipo de simulación.\n"); return 0;}
+
+    char *tipo = argv[1];
+
+    pthread_t hiloCreador;
+
+    if(tipo=="1")//1:Paginacipn  2:Segmentacion
+        //pthread_create (&hiloCreador, NULL, creadorProcesos, NULL;
+        isSEGMENTATION=0;
+
+    else if (tipo=="2")
+        //pthread_create (&hiloCreador, NULL, creadorProcesos, NULL);
+        isSEGMENTATION=1;
+    else{
+        printf("ERROR: Debe ingresar un modo de simulación válido (1-2).\n");
+        return 0;
+    }
+
+    //pthread_join(hiloCreador, NULL);
+
+    printf("\n\tSTATUS: El creador de proceso se ha detenido.\n");
+    return 0;
+}
